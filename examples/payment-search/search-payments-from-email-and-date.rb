65325b452a5b8457aa463d5ed993d68184bd9543
Matias Compiano <matias.compiano@mercadolibre.com>
2013-04-25 15:32:35 -0300

    Fix en el uso de Hash[]

--- examples/payment-search/search-payments-from-email-and-date.rb
+++ examples/payment-search/search-payments-from-email-and-date.rb
@@ -8,7 +8,7 @@ class IPN
 	mp = MercadoPago.new('CLIENT_ID', 'CLIENT_SECRET')
 	
 	# Sets the filters you want
-	filters = Hash[Array["payer_email" => "mail02@mail02.com%20mail01@mail01.com", "begin_date"=>"2011-01-01T00:00:00Z", "end_date"=>"2011-02-01T00:00:00Z"]]
+	filters = Hash["payer_email" => "mail02@mail02.com%20mail01@mail01.com", "begin_date"=>"2011-01-01T00:00:00Z", "end_date"=>"2011-02-01T00:00:00Z"]
 	
 	# Search payment data according to filters
 	searchResult = mp.search_payment(filters)
@@ -21,10 +21,3 @@ class IPN
 end
 
 Rack::Handler::WEBrick.run(IPN.new, :Port => 9000)
-
-
-        $filters = array (
-            "payer_email" => "mail02@mail02.com%20mail01@mail01.com",
-            "begin_date" => "2011-01-01T00:00:00Z",
-            "end_date" => "2011-02-01T00:00:00Z"
-        );
