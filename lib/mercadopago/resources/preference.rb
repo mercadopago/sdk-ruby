7337349bb9c9b0bdadd477b9af9efb3e38b980da
michel <michel.marciano@mercadolivre.com>
2020-12-08 17:18:13 -0300

    inclusion put preference

--- lib/mercadopago/resources/preference.rb
+++ lib/mercadopago/resources/preference.rb
@@ -4,16 +4,17 @@ module Mercadopago
             super(request_options, http_client)
         end
 
-        def get(request_options:nil)
-            _get(uri:"/checkout/preferences", request_options:request_options)
+        def get(preference_id=nil, request_options:nil)
+            _get(uri:"/checkout/preferences/#{preference_id}", request_options: request_options)
         end
 
         def post(data, request_options:nil)
             _post(uri:"/checkout/preferences", data:data, request_options:request_options)
         end
 
-        def put(data, request_options:nil)
-            _put(uri:"/checkout/preferences", data:data, request_options:request_options)
+        def put(preference_id, data, request_options:nil)
+            puts data
+            _put(uri:"/checkout/preferences/#{preference_id}", data:data, request_options:request_options)
         end
 
     end
--- tests/test_preference.rb
+++ tests/test_preference.rb
@@ -5,13 +5,19 @@ require 'minitest/autorun'
 class TestPreference < Minitest::Test
     
     def test_method_get
-       sdk = Mercadopago::SDK.new(access_token="APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966")
+       sdk = Mercadopago::SDK.new(access_token="TEST-783169576377080-082620-395ee7f82e0d55b1db606c118686c1db-464842924")
 		result = sdk.preference().get()
+        assert_equal 200, result[:status]
+    end
+
+    def test_method_get_id
+        sdk = Mercadopago::SDK.new(access_token="TEST-783169576377080-082620-395ee7f82e0d55b1db606c118686c1db-464842924")
+        result = sdk.preference().get(preference_id="464842924-68bdeddf-5642-44ef-853c-e0d4df3a63f1")
         assert_equal 200, result[:status]
     end
     
     def test_method_post
-       sdk = Mercadopago::SDK.new(access_token="APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966")
+       sdk = Mercadopago::SDK.new(access_token="TEST-783169576377080-082620-395ee7f82e0d55b1db606c118686c1db-464842924")
        data = {
         "items": [
             {
@@ -28,21 +34,19 @@ class TestPreference < Minitest::Test
     end
 
     def test_method_put
-        sdk = Mercadopago::SDK.new(access_token="APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966")
-        data = {
-         "items": [
-             {
-             "title": "Dummy Items",
-             "description": "Barcelona Items",
-             "quantity": 1,
-             "currency_id": "",
-             "unit_price": 10.0
-             }
-          ]
-       }
-        result = sdk.preference().put(data)
-        assert_equal 201, result[:status]
-     end
-    
-
+       sdk = Mercadopago::SDK.new(access_token="TEST-783169576377080-082620-395ee7f82e0d55b1db606c118686c1db-464842924")
+       data = {
+             "items": [
+                {
+                "title": "Dummy Item",
+                "description": "Multicolor Item",
+                "quantity": 1,
+                "currency_id": "",
+                "unit_price": 10.0
+                }
+            ]
+         }
+        result = sdk.preference().put(preference_id="464842924-68bdeddf-5642-44ef-853c-e0d4df3a63f1", data)
+        assert_equal 200, result[:status]
+    end
 end
