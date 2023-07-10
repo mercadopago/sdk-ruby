2592cb12d97044da4d6d7bd4ffc875cfbaed1268
Romero Silva <romero.ca.silva@gmail.com>
2023-07-10 16:04:57 -0300

    Refactor tests
    to remove references to sensitive data

--- tests/test_preapproval.rb
+++ tests/test_preapproval.rb
@@ -6,24 +6,8 @@ require_relative '../lib/mercadopago'
 require 'minitest/autorun'
 
 class TestPreapproval < Minitest::Test
-  def test_method_search
-    sdk = Mercadopago::SDK.new('APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966')
-    result = sdk.preapproval.search(filters: { id: '2c938084884df89301884fecf62e012a' })
-
-    assert_equal 200, result[:status]
-  end
-
-  def test_method_get_id
-    sdk = Mercadopago::SDK.new('APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966')
-    result = sdk.preapproval.get('2c938084884df89301884fecf62e012a')
-
-    assert_equal 200, result[:status]
-  end
-
-  def test_method_post
-    sdk = Mercadopago::SDK.new('APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966')
-
-    card_token_object = {
+  def setup
+    @card_token_object = {
       card_number: '5031433215406351',
       expiration_year: 2025,
       expiration_month: 11,
@@ -32,11 +16,62 @@ class TestPreapproval < Minitest::Test
         name: 'APRO'
       }
     }
+  end
 
-    result_card_token = sdk.card_token.create(card_token_object)
+  def test_method_search
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+
+    create_response = create_preapproval(sdk)
+    assert_equal 201, create_response[:status]
+
+    result = sdk.preapproval.search(filters: { id: create_response[:response]['id'] })
+    assert_equal 200, result[:status]
+  end
+
+  def test_method_get_id
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+
+    create_response = create_preapproval(sdk)
+    assert_equal 201, create_response[:status]
+
+    result = sdk.preapproval.get(create_response[:response]['id'])
+    assert_equal 200, result[:status]
+  end
+
+  def test_method_post
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+
+    result = create_preapproval(sdk)
+    assert_equal 201, result[:status]
+  end
+
+  def test_method_put
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+
+    create_response = create_preapproval(sdk)
+    assert_equal 201, create_response[:status]
+
+    update_data = {
+      reason: 'Yoga classes',
+      external_reference: 'YG-12345',
+      payer_email: 'test_user@testuser.com',
+      auto_recurring: {
+        frequency: 2,
+        frequency_type: 'months',
+        transaction_amount: 15,
+        currency_id: 'BRL'
+      },
+      back_url: 'https://www.mercadopago.com.ar',
+      status: 'authorized'
+    }
+    result = sdk.preapproval.update(create_response[:response]['id'], update_data)
+    assert_equal 200, result[:status]
+  end
+
+  def create_preapproval(sdk)
+    result_card_token = sdk.card_token.create(@card_token_object)
 
     preapproval_data = {
-      preapproval_plan_id: '2c938084884cac3001884fea32fe014a',
       reason: 'Yoga classes',
       external_reference: 'YG-1234',
       payer_email: 'test_user@testuser.com',
@@ -53,41 +88,6 @@ class TestPreapproval < Minitest::Test
       status: 'authorized'
     }
 
-    result = sdk.preapproval.create(preapproval_data)
-    assert_equal 201, result[:status]
-  end
-
-  def test_method_put
-    sdk = Mercadopago::SDK.new('APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966')
-
-    card_token_object = {
-      card_number: '5031433215406351',
-      expiration_year: 2025,
-      expiration_month: 11,
-      security_code: '123',
-      cardholder: {
-        name: 'APRO'
-      }
-    }
-
-    result_card_token = sdk.card_token.create(card_token_object)
-
-    preapproval_data = {
-      preapproval_plan_id: '2c938084884cac3001884fea32fe014a',
-      reason: 'Yoga classes',
-      external_reference: 'YG-12345',
-      payer_email: 'test_user@testuser.com',
-      card_token_id: result_card_token[:response]['id'],
-      auto_recurring: {
-        frequency: 2,
-        frequency_type: 'months',
-        transaction_amount: 15,
-        currency_id: 'BRL'
-      },
-      back_url: 'https://www.mercadopago.com.ar',
-      status: 'authorized'
-    }
-    result = sdk.preapproval.update('2c938084884df89301884fecf62e012a', preapproval_data)
-    assert_equal 200, result[:status]
+    sdk.preapproval.create(preapproval_data)
   end
 end
--- tests/test_preapproval_plan.rb
+++ tests/test_preapproval_plan.rb
@@ -6,24 +6,8 @@ require_relative '../lib/mercadopago'
 require 'minitest/autorun'
 
 class TestPreapprovalPlan < Minitest::Test
-  def test_method_search
-    sdk = Mercadopago::SDK.new('TEST-6130770563612470-121314-d27bbd7363e64c3853f058251cf8fc6e-537031659')
-    result = sdk.preapproval_plan.search(filters: { id: '2c9380848848983b01884f8458d70324' })
-
-    assert_equal 200, result[:status]
-  end
-
-  def test_method_get_id
-    sdk = Mercadopago::SDK.new('TEST-6130770563612470-121314-d27bbd7363e64c3853f058251cf8fc6e-537031659')
-    result = sdk.preapproval_plan.get('2c9380848848983b01884f8458d70324')
-
-    assert_equal 200, result[:status]
-  end
-
-  def test_method_post
-    sdk = Mercadopago::SDK.new('TEST-6130770563612470-121314-d27bbd7363e64c3853f058251cf8fc6e-537031659')
-
-    preapproval_plan_data = {
+  def setup
+    @preapproval_plan_data = {
       reason: 'Yoga classes',
       auto_recurring: {
         frequency: 1,
@@ -48,14 +32,38 @@ class TestPreapprovalPlan < Minitest::Test
       },
       back_url: 'https://www.yoursite.com'
     }
+  end
+
+  def test_method_search
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+    result = sdk.preapproval_plan.create(@preapproval_plan_data)
+    assert_equal 201, result[:status]
+
+    result = sdk.preapproval_plan.search(filters: { id: result[:response]['id'] })
+    assert_equal 200, result[:status]
+  end
+
+  def test_method_get_id
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+
+    result = sdk.preapproval_plan.create(@preapproval_plan_data)
+    assert_equal 201, result[:status]
+
+    result = sdk.preapproval_plan.get(result[:response]['id'])
+    assert_equal 200, result[:status]
+  end
+
+  def test_method_post
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
     
-    result = sdk.preapproval_plan.create(preapproval_plan_data)
+    result = sdk.preapproval_plan.create(@preapproval_plan_data)
     assert_equal 201, result[:status]
   end
 
   def test_method_put
-    sdk = Mercadopago::SDK.new('TEST-6130770563612470-121314-d27bbd7363e64c3853f058251cf8fc6e-537031659')
-    preapproval_plan_data = {
+    sdk = Mercadopago::SDK.new(ENV['ACCESS_TOKEN'])
+
+    update_data = {
       reason: 'Pilates classes',
       auto_recurring: {
         frequency: 1,
@@ -80,7 +88,11 @@ class TestPreapprovalPlan < Minitest::Test
       },
       back_url: 'https://www.yoursite.com'
     }
-    result = sdk.preapproval_plan.update('2c9380848848983b01884f8458d70324', preapproval_plan_data)
+
+    result = sdk.preapproval_plan.create(@preapproval_plan_data)
+    assert_equal 201, result[:status]
+
+    result = sdk.preapproval_plan.update(result[:response]['id'], update_data)
     assert_equal 200, result[:status]
   end
 end
