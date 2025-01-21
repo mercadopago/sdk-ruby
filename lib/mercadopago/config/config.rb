fa638024e50698c9d1b981b063ddfb9fdbf2fe6b
Kelvin Carvalho <kelvin.carvalho@mercadolivre.com>
2025-01-21 13:53:28 -0300

    docs: upgrade

--- lib/mercadopago/config/config.rb
+++ lib/mercadopago/config/config.rb
@@ -3,7 +3,7 @@
 
 module Mercadopago
   class Config
-    @@VERSION = '2.2.1'
+    @@VERSION = '2.3.0'
     @@USER_AGENT = "MercadoPago Ruby SDK v#{@@VERSION}"
     @@PRODUCT_ID = 'bc32a7vtrpp001u8nhjg'
     @@TRACKING_ID =  "platform:#{RUBY_VERSION},type:SDK#{@@VERSION},so;"
